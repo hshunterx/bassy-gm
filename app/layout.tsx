ff8eb46a79539f3ba273fdf470aa93b4212111f2
hshunterx <herdy.salim.hs@gmail.com>
2026-02-02 21:10:39 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Providers } from "@/components/Providers";
+// Jalur ini mengasumsikan folder 'components' ada di luar folder 'app'
+import { Providers } from "../components/Providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
